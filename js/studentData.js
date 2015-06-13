e00f298a4104be05db9c9f91d29af3483cd423fe
lorak <k.zysk@zoho.com>
2015-06-13 15:11:42 +0200

    move student data to js file

---
+++ js/studentData.js
@@ -0,0 +1,19 @@
+var data = {
+    	"students": [
+    	{
+         	"name": "Slappy the Frog"
+        },
+    	{
+    		"name": "Lilly the Lizard"
+    	},
+    	{
+    		"name": "Paulrus the Walrus"
+        },
+        {
+    	    "name": "Gregory the Goat"
+    	},
+        {
+    	    "name":  "Adam the Anaconda"
+    	}
+    ]
+};
